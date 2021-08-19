f3472317c91a7fa83b17de3c28c737e89fa5d35c
Colin <colin.diesh@gmail.com>
2021-08-19 09:20:40 -0400

    Add arcrenderer

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,24 @@
 import Plugin from '@jbrowse/core/Plugin'
 import PluginManager from '@jbrowse/core/PluginManager'
-import { isAbstractMenuManager } from '@jbrowse/core/util'
 import { version } from '../package.json'
-import { ReactComponent } from './HelloView'
+
+import ArcRenderer, {
+  configSchema as ArcRendererConfigSchema,
+  ReactComponent as ArcRendererReactComponent,
+} from './ArcRenderer'
 
 export default class MyProjectPlugin extends Plugin {
-  name = 'MyProject'
   version = version
-
+  name = 'ArcRenderer'
   install(pluginManager: PluginManager) {
-    const { jbrequire } = pluginManager
-    const { types } = pluginManager.lib['mobx-state-tree']
-
-    const ViewType = jbrequire('@jbrowse/core/pluggableElementTypes/ViewType')
-    const stateModel = types
-      .model({ type: types.literal('HelloView') })
-      .actions(() => ({
-        setWidth() {
-          // unused but required by your view
-        },
-      }))
-
-    pluginManager.addViewType(() => {
-      return new ViewType({ name: 'HelloView', stateModel, ReactComponent })
-    })
-  }
-
-  configure(pluginManager: PluginManager) {
-    if (isAbstractMenuManager(pluginManager.rootModel)) {
-      // @ts-ignore
-      pluginManager.rootModel.appendToSubMenu(['File', 'Add'], {
-        label: 'Open Hello!',
-        // @ts-ignore
-        onClick: session => {
-          session.addView('HelloView', {})
-        },
-      })
-    }
+    pluginManager.addRendererType(
+      () =>
+        // @ts-ignore error "expected 0 arguments, but got 1"?
+        new ArcRenderer({
+          name: 'ArcRenderer',
+          ReactComponent: ArcRendererReactComponent,
+          configSchema: ArcRendererConfigSchema,
+        }),
+    )
   }
 }
